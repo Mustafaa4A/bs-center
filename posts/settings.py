47a7203325f333e18db552d512c11b347c14506a
mustafa42Abubakar <musdhafa42@gmail.com>
2020-09-29 21:31:38 +0300

    new template

--- posts/settings.py
+++ posts/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '1#xg&vmkd0b!$vv*&@nb-+)^k-$2v19-e^j(d5lcc*u8myu@#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bs-center.herokuapp.com']
 
