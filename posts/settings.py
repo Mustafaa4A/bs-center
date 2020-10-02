97239f747702e02d1fa9420b82528051e50f5909
mustafa42Abubakar <musdhafa42@gmail.com>
2020-10-02 11:32:10 +0300

    django-heroku

--- posts/settings.py
+++ posts/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1#xg&vmkd0b!$vv*&@nb-+)^k-$2v19-e^j(d5lcc*u8myu@#2'
-
+# SECRET_KEY = '1#xg&vmkd0b!$vv*&@nb-+)^k-$2v19-e^j(d5lcc*u8myu@#2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -150,4 +150,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # django_heroku.settings(locals())
-#
